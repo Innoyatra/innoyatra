1f63f80c078a1f1917f8a2f6be95231b0c40d0a2
Keerthana430 <keerthanakrisuj@gmail.com>
2024-12-22 15:55:44 +0530

    feat: create a hero page  of the website

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,33 @@
 
+import Image from 'next/image'
+
 const Navbar = () => {
   return (
-   <nav className="flex justify-between p-5">
-    <div>
-        Innoyatra
-    </div>
-    <ul className="flex gap-4">
-        <li>Home</li>
-        <li>services</li>
-        <li>contact</li>
-        <li>about us</li>
-    </ul>
-    <button className="mr-4">Contact us</button>
-   </nav>
+    <header className="bg-white shadow-sm">
+      <nav className="container mx-auto flex items-center justify-between p-5">
+        <div className="flex items-center">
+          <Image 
+            src="/Logo.svg"
+            alt="Innoyatra Logo" 
+            width={40} 
+            height={40}
+            className="mr-2"
+          />
+          <span className="text-xl font-bold">Innoyatra</span>
+        </div>
+        <ul className="flex gap-12">
+          <li className="hover:text-gray-600">Home</li>
+          <li className="hover:text-gray-600">Services</li>
+          <li className="hover:text-gray-600">Team</li>
+          <li  className="hover:text-gray-600">Blog</li>
+          <li  className="hover:text-gray-600">How it works</li>
+          <li  className="hover:text-gray-600">Hire</li>
+        </ul>
+        <button className="rounded-lg bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 ">
+          Contact Us
+        </button>
+      </nav>
+    </header>
   )
 }
 
